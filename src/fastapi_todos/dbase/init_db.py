6f1b7135749c3fe6194868aedfbf62d45bcfd490
grithaqq <grithaqq@gmail.com>
2024-04-06 01:27:05 +0800

    feat: porting dbase package complete #2

---
+++ src/fastapi_todos/dbase/init_db.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Session
+
+from core.config import settings
+from user import repository  # noqa: F401
+from user import schemas
+
+# make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
+# otherwise, SQL Alchemy might fail to initialize relationships properly
+# for more details: https://github.com/tiangolo/full-stack-fastapi-postgresql/issues/28
+
+
+def init_db(db: Session) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next line
+    # Base.metadata.create_all(bind=engine)
+    user_repo = repository.UserV2Repository(db)
+
+    user = user_repo.get_by_email(email=settings.FIRST_SUPERUSER)
+    if not user:
+        user_in = schemas.UserCreate(
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        user = user_repo.create(obj_in=user_in)  # noqa: F841
